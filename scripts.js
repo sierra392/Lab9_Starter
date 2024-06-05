f849cfdceb63b49161ac4808037eaceb685f327b
sierra392 <ssm24816@gmail.com>
2024-06-04 22:02:51 -0700

    finished lab

---
+++ scripts.js
@@ -0,0 +1,5 @@
+let errorBtns = Array.from(document.querySelectorAll('#error-btns > button'));
+errorBtns[0].addEventListener("click", () => {
+    console.log("?")
+    //console.log(document.querySelector('#first-num') + document.querySelector('#operator') + document.querySelector('#second-num'));
+  });
